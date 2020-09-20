e1c316a46e1dd9f0dfd7db9c848171a33aa7d425
kwokwai6618 <58355825+kwokwai6618@users.noreply.github.com>
2020-09-20 19:54:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,21 +9,27 @@
 #=============================================================
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # add argon theme
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 rm -rf package/lean/luci-theme-argon && mv luci-theme-argon package/lean/
+
+# add argon-config
+git clone https://github.com/jerrykuku/luci-app-argon-config.git
+mv luci-app-argon-config package/lean/
 
 # add vssr
 git clone https://github.com/jerrykuku/lua-maxminddb.git
 mv lua-maxminddb package/lean/
 git clone https://github.com/jerrykuku/luci-app-vssr.git
 mv luci-app-vssr package/lean/
+rm -rf package/lean/luci-app-vssr/root/etc/china_ssr.txt
+rm -rf package/lean/luci-app-vssr/root/etc/dnsmasq.oversea/oversea_list.conf
+rm -rf package/lean/luci-app-vssr/root/etc/dnsmasq.ssr/gfw_base.conf
+rm -rf package/lean/luci-app-vssr/root/etc/dnsmasq.ssr/gfw_list.conf
 
 # add JD
-git clone https://github.com/jerrykuku/node-request.git
-mv node-request package/lean/
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git
 mv luci-app-jd-dailybonus package/lean/
 
@@ -33,6 +39,9 @@ mv luci-app-adguardhome package/lean/
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# Add Mattraks helloworld
+sed -i '$a src-git helloworld https://github.com/Mattraks/helloworld' feeds.conf.default
 
 # Add openclash
 # echo "src-git openclash https://github.com/vernesong/OpenClash;master" >> feeds.conf.default
